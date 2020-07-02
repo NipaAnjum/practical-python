3e574fe15da0c88a3619fa17bc9c97080fc30ebe
NipaAnjum <nipaanjum1998@gmail.com>
2020-07-02 12:06:33 +0600

    Work\starter.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+h = 100
+i = 1
+while i<=10:
+    h = h * (3/5)
+    print(i, round(h,4))
+    i = i+1
+
---
+++ Work/starter.py
@@ -0,0 +1,54 @@
+#1
+# bill_thickness = 0.11 * 0.001
+# sear_height = 442
+# bill_num = 1
+# day = 1
+# while bill_num * bill_thickness < sear_height:
+#     print(day, bill_num, bill_num * bill_thickness)
+#     day = day+1
+#     bill_num = bill_num * 2
+# print (bill_num, day, bill_num * bill_thickness)
+
+#2
+# x + y      Add
+# x - y      Subtract
+# x * y      Multiply
+# x / y      Divide (produces a float)
+# x // y     Floor Divide (produces an integer)
+# x % y      Modulo (remainder)
+# x ** y     Power
+# x << n     Bit shift left
+# x >> n     Bit shift right
+# x & y      Bit-wise AND
+# x | y      Bit-wise OR
+# x ^ y      Bit-wise XOR
+# ~x         Bit-wise NOT
+# abs(x)     Absolute value
+# a = 37
+# b = -299392993727716627377128481812241231
+# c = 0x7fa8      # Hexadecimal
+# d = 0o253       # Octal
+# e = 0b10001111  # Binary
+
+#3
+# import math
+# a = math.sqrt(x)
+# b = math.sin(x)
+# c = math.cos(x)
+# d = math.tan(x)
+# e = math.log(x)
+
+# 4 comparison
+# if b >= a and b <= c:
+#     print('b is between a and c')
+
+# if not (b < a or b > c):
+#     print('b is still between a and c')
+
+# 5
+# x = 3.4521
+# # a = int(x)
+# b = '4.5'
+# c = float(b)
+
+
